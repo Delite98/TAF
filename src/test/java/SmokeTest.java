49545db6730657b2382c1777c9cacb5071b760dc
Darya <dashkash22@mail.ru>
2022-04-20 16:33:28 +0300

    Lesson_Selenium_2

--- src/test/java/SmokeTest.java
+++ src/test/java/SmokeTest.java
@@ -45,6 +45,26 @@ public class SmokeTest {
         Thread.sleep(2000);
         selectSex.selectByVisibleText("женский");
         Thread.sleep(2000);
+        WebElement creatin = driver.findElement(By.id("oCr"));
+        creatin.sendKeys("80");
+        WebElement age = driver.findElement(By.id("oAge"));
+        age.sendKeys("38");
+        WebElement weight = driver.findElement(By.id("oWeight"));
+        weight.sendKeys("55");
+        WebElement height = driver.findElement(By.id("oHeight"));
+        height.sendKeys("163");
+        WebElement button = driver.findElement(By.xpath("//input[@value='Рассчитать']"));
+        button.click();
+
+        WebElement mdrd = driver.findElement(By.id("txtMDRD"));
+        Assert.assertEquals(mdrd.getText(), "MDRD: 74 (мл/мин/1,73кв.м)");
+        WebElement xbp = driver.findElement(By.id("txtMDRD1"));
+        Assert.assertEquals(xbp.getText(),"ХБП: 2 стадия (при наличии почечного повреждения)");
+        WebElement cockroftG = driver.findElement(By.id("txtCG"));
+        Assert.assertEquals(cockroftG.getText(), "Cockroft-Gault: 70 (мл/мин)");
+        WebElement body = driver.findElement(By.id("txtBSA"));
+        Assert.assertEquals(body.getText(), "Поверхность тела:1.58 (кв.м)");
+        Thread.sleep (2000);
     }
     @AfterMethod
     public void tearDown(){
